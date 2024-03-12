f0ca24c24ab29c750ee81efabfce7f59e6989df7
Ben Pearce <me@benpearce.io>
2024-03-12 23:08:24 +0000

    fix: :adhesive_bandage: fixing container terraform defs

--- modules/containers/jellyfin-ffmpeg.tf
+++ modules/containers/jellyfin-ffmpeg.tf
@@ -1,12 +1,28 @@
 resource "proxmox_virtual_environment_container" "jellyfin_ffmpeg" {
-  node_name = "pve"
-  vm_id     = 1000
-  tags      = ["internal", "jammy", "lxc", "ubuntu"]
+  node_name   = "pve"
+  vm_id       = 1000
+  tags        = ["internal", "jammy", "lxc", "ubuntu"]
+
+  initialization {
+    hostname = "jellyfin-ffmpeg"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
 
   clone {
-    node_name = "pve"
-    datastore_id = "local"
-    vm_id = proxmox_virtual_environment_container.ubuntu_jammy_template.id
+    node_name     = "pve"
+    datastore_id  = "local-lvm"
+    vm_id         = proxmox_virtual_environment_container.lxc_ubuntu_jammy_template.id
+  }
+
+  startup {
+    order      = "3"
+    up_delay   = "60"
+    down_delay = "60"
   }
 
   cpu {
@@ -17,20 +33,10 @@ resource "proxmox_virtual_environment_container" "jellyfin_ffmpeg" {
     dedicated = 12288
   }
 
-  features {
-    fuse  = true
-    mount = ["nfs"]
-  }
-
   network_interface {
     name        = "eth0"
     vlan_id     = 10
     mac_address = "02:00:00:00:10:00"
-  }
-
-  operating_system {
-    template_file_id = proxmox_virtual_environment_file.ubuntu_container_template.id
-    type             = "ubuntu"
   }
 
   disk {
