9fe58dab3d41a4b567ff45fb377412a981a453f6
Ben Pearce <me@benpearce.io>
2024-05-14 20:56:05 +0100

    feat(rc): :sparkles: add persistent disk to rc

--- modules/servers/rc.tf
+++ modules/servers/rc.tf
@@ -13,7 +13,7 @@ resource "proxmox_virtual_environment_vm" "rc_data" {
     datastore_id = "local-lvm"
     interface    = "virtio0"
     file_format  = "raw"
-    size         = 5
+    size         = 10
   }
 
   lifecycle {
@@ -28,6 +28,43 @@ resource "proxmox_virtual_environment_vm" "rc" {
   node_name = "pve"
   vm_id     = 201
   bios      = "ovmf"
+
+  initialization {
+    vendor_data_file_id = proxmox_virtual_environment_file.cloud_config_vdb.id
+    interface = "scsi0"
+
+    user_account {
+      keys     = [trimspace(file(var.public_key_file))]
+      password = random_password.ubuntu_vm_password.result
+      username = var.default_user
+    }
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  agent {
+    enabled = true
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  startup {
+    order      = "1"
+    up_delay   = "60"
+    down_delay = "60"
+  }
+
+  efi_disk {
+    datastore_id      = "local-lvm"
+    type              = "4m"
+    pre_enrolled_keys = true
+  }
 
   clone {
     vm_id = proxmox_virtual_environment_vm.ubuntu_noble_template.id
@@ -48,7 +85,21 @@ resource "proxmox_virtual_environment_vm" "rc" {
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
-    size         = 64
+    size         = 5
+  }
+
+  dynamic "disk" {
+    for_each = { for idx, val in proxmox_virtual_environment_vm.rc_data.disk : idx => val }
+    iterator = data_disk
+    content {
+      datastore_id      = data_disk.value["datastore_id"]
+      path_in_datastore = data_disk.value["path_in_datastore"]
+      file_format       = data_disk.value["file_format"]
+      size              = data_disk.value["size"]
+      interface         = "virtio${data_disk.key + 1}"
+      iothread          = true
+      discard           = "on"
+    }
   }
 
   network_device {
@@ -61,5 +112,12 @@ resource "proxmox_virtual_environment_vm" "rc" {
     command = "ansible-playbook -u ${var.default_user} --private-key ${var.private_key_file} ansible-playbooks/rc.yml"
   }
 
-  depends_on = [ proxmox_virtual_environment_vm.barbie ]
+  lifecycle {
+    ignore_changes = [ clone ]
+  }
+
+  depends_on = [ 
+    proxmox_virtual_environment_vm.rc_data,
+    proxmox_virtual_environment_vm.barbie
+  ]
 }
