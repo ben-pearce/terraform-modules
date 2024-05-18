fef2c0e2fbadeda4c372bf92428de5df6962632c
Ben Pearce <me@benpearce.io>
2024-05-18 23:20:20 +0100

    chore: :wrench: update truenas ISO

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -13,7 +13,7 @@ resource "proxmox_virtual_environment_file" "truenas_iso" {
   node_name    = "pve"
 
   source_file {
-    path = "https://download-core.sys.truenas.net/13.0/STABLE/U5.3/x64/TrueNAS-13.0-U5.3.iso"
+    path = "https://download-core.sys.truenas.net/13.0/STABLE/U6.1/x64/TrueNAS-13.0-U6.1.iso"
   }
 }
 
