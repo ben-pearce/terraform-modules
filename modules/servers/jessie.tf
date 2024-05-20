dd35e5d022fa8440c4966a3cce93da1c4fb97672
Ben Pearce <me@benpearce.io>
2024-05-20 19:40:03 +0100

    feat(jessie): :sparkles: add persistent disk

--- modules/servers/jessie.tf
+++ modules/servers/jessie.tf
@@ -25,9 +25,23 @@ resource "proxmox_virtual_environment_vm" "jessie" {
   name        = "jessie"
   tags        = ["internal", "jammy", "ubuntu"]
 
-  node_name = "pve"
-  vm_id     = 100
-  bios      = "ovmf"
+
+  initialization {
+    vendor_data_file_id = proxmox_virtual_environment_file.cloud_config_vdb.id
+    interface = "scsi0"
+
+    user_account {
+      keys     = [trimspace(file(var.public_key_file))]
+      password = random_password.ubuntu_vm_password.result
+      username = var.default_user
+    }
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
 
   clone {
     vm_id = proxmox_virtual_environment_vm.ubuntu_jammy_template.id
@@ -55,7 +69,21 @@ resource "proxmox_virtual_environment_vm" "jessie" {
     interface    = "virtio0"
     iothread     = true
     discard      = "on"
-    size         = 120
+    size         = 64
+  }
+
+  dynamic "disk" {
+    for_each = { for idx, val in proxmox_virtual_environment_vm.jessie_data.disk : idx => val }
+    iterator = data_disk
+    content {
+      datastore_id      = data_disk.value["datastore_id"]
+      path_in_datastore = data_disk.value["path_in_datastore"]
+      file_format       = data_disk.value["file_format"]
+      size              = data_disk.value["size"]
+      interface         = "virtio${data_disk.key + 1}"
+      iothread          = true
+      discard           = "on"
+    }
   }
 
   network_device {
@@ -68,5 +96,8 @@ resource "proxmox_virtual_environment_vm" "jessie" {
     command = "ansible-playbook -u ${var.default_user} --private-key ${var.private_key_file} ansible/jessie.yml"
   }
 
-  depends_on = [ proxmox_virtual_environment_vm.barbie ]
+  depends_on = [ 
+    proxmox_virtual_environment_vm.jessie_data,
+    proxmox_virtual_environment_vm.barbie 
+  ]
 }
